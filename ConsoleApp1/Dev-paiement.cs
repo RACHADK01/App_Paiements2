7489d5f487901e0e74b31594a6687f7f8e37d2dc
Rachad <300140175@monboreal.ca>
2024-11-06 12:18:18 -0500

    Ajout de la classe Paiement dans la branche dev-paiement

---
+++ ConsoleApp1/Dev-paiement.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class Dev_paiement
+    {
+        //Initialisation des propriétés
+        public double Montant { get; set; }
+        public string Description { get; set; }
+        //Initialisation du constructeur 
+        public Dev_paiement(double montant, string description) 
+        {
+            Montant = montant;
+            Description = description;
+        }
+        //Initialisation de la méthode 
+        public virtual void AfficherDetails()
+        {
+            Console.WriteLine($"Montant:{Montant},Description:{Description}");
+        }
+
+    }
+}
