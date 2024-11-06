77c44e7dd10ddcc47fe16c5fc59c5502ad9f9ca6
Rachad <300140175@monboreal.ca>
2024-11-06 12:43:12 -0500

    Mise a jour de classe Program.cs

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -1,7 +1,17 @@
-﻿internal class Program
+﻿using ConsoleApp1;
+
+internal class Program
 {
     private static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+        // Instances de Carte de Credit
+        CarteCredit achat_CC1 = new CarteCredit(156.00, "Achat de livres", 467887346);
+        CarteCredit achat_CC2 = new CarteCredit(226.43, "Achat d'équipement", 987654321);
+        CarteCredit achat_CC3 = new CarteCredit(50.50, "Repas au restaurant", 456789123);
+
+        achat_CC1.AfficherDetails();
+        achat_CC2.AfficherDetails();
+        achat_CC3.AfficherDetails();
+
     }
 }
